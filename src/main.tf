8e220c1758cc4ddfb6191901888b245b2a17817b
lchikkala28 <lokeshchikkala.cas02@outlook.com>
2025-02-15 11:55:42 +0530

    updated recover_soft_deleted_key_vaults to true

--- src/main.tf
+++ src/main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
   # Configuration options  
   features {
     key_vault {
-      recover_soft_deleted_key_vaults    = false
+      recover_soft_deleted_key_vaults    = true
       purge_soft_delete_on_destroy       = false
       purge_soft_deleted_keys_on_destroy = false
     }
